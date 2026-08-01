5794526b156a7700e644844b6204ec93f8bb39e7
Kanishka Sharma <k.sharma16062007@gmail.com>
2026-08-01 21:31:57 +0530

    updated views with deletion and updation of tweets methods

--- Chirps/tweet/views.py
+++ Chirps/tweet/views.py
@@ -21,3 +21,25 @@ def tweet_create(request):
     else:
         form=TweetForm()
     return render(request,'tweet_form.html',{'form':form})
+
+    def tweet_edit(request,tweet_id):
+         tweet=get_object_or_404(Tweet,pk=tweet_id,user=request.user)
+         if request.method=='POST':
+             form=TweetForm(request.POST,request.FILES,instance=tweet)
+             if form.is_valid():
+                 tweet=form.save(commit=false)
+                 tweet.user=request.user
+                 tweet.save()
+                 return redirect('tweet_list')
+         else:
+             form=TweetForm(instance=tweet)
+         return render(request,'tweet_form.html',{'form':form})
+
+    def tweet_delete(request,tweet_id):
+        get_object_or_404(Tweet,pk=tweet_id,user=request.user)
+        if request.method=='POST':
+            tweet.delete()
+            return redirect('tweet_list')
+        return render(request,'tweet_confirm_delete.html',{'tweet':tweet})
+        
+             
