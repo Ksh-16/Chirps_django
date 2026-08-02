fdc6074c6a78d6804449ad47912ea37270c27c0a
Kanishka Sharma <k.sharma16062007@gmail.com>
2026-08-02 11:47:39 +0530

    updated app templates files and urls.py

--- Chirps/tweet/urls.py
+++ Chirps/tweet/urls.py
@@ -2,5 +2,8 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.tweet, name='tweet_list'),
+    path('create/', views.create, name='tweet_create'),
+    path('<int:tweet_id>/delete/', views.delete, name='tweet_delete'),
+    path('<int:tweet_id>/edit/', views.edit, name='tweet_edit'),
 ] 
