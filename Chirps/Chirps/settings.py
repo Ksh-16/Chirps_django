da21fe471dec97e50104fb39718b56f6145b962c
Kanishka Sharma <k.sharma16062007@gmail.com>
2026-08-26 18:59:38 +0530

    updated settings.py for security

--- Chirps/Chirps/settings.py
+++ Chirps/Chirps/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-+zidg1u4jx*w(caa=ms9ip*ph8lt7l_lse(66ctz@$jg1t=_gz')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("...")
+
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,Ksh16.pythonanywhere.com').split(',')
